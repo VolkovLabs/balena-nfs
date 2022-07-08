8344d8b90054378640fea28309a80c1b6f3804e8
Mikhail Volkov <47795110+mikhail-vl@users.noreply.github.com>
2022-07-08 14:31:56 -0400

    Separate NFS Mount from local Mount Point (#9)
    * Separate NFS Mount from local Mount Point
    
    * Update CHANGELOG

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -d "${NFS_MOUNT_POINT}" ]; then
     mkdir -p ${NFS_MOUNT_POINT}
 fi
 
-echo "${NFS_HOST}:${NFS_MOUNT_POINT} ${NFS_MOUNT_POINT} ${NFS_VERSION} hard,nolock 0 0" > /etc/fstab
+echo "${NFS_HOST}:${NFS_HOST_MOUNT} ${NFS_MOUNT_POINT} ${NFS_VERSION} hard,nolock 0 0" > /etc/fstab
 
 echo "Mounting NFS = ${NFS_MOUNT_POINT}..."
 while ( ! df | grep "${NFS_HOST}:${NFS_MOUNT_POINT}" > /dev/null; ) && [ ! "${NFS_MOUNT_SKIP}" ]; do
