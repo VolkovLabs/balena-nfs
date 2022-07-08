6104ecb40b7f48c8d0486722ab7e1a628d8a2647
Mikhail <mikhail@volkovlabs.io>
2022-07-08 15:03:13 -0400

    Update NFS validation to use new Environmet Variable

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -8,7 +8,7 @@ fi
 echo "${NFS_HOST}:${NFS_HOST_MOUNT} ${NFS_MOUNT_POINT} ${NFS_VERSION} hard,nolock 0 0" > /etc/fstab
 
 echo "Mounting NFS = ${NFS_MOUNT_POINT}..."
-while ( ! df | grep "${NFS_HOST}:${NFS_MOUNT_POINT}" > /dev/null; ) && [ ! "${NFS_MOUNT_SKIP}" ]; do
+while ( ! df | grep "${NFS_HOST}:${NFS_HOST_MOUNT}" > /dev/null; ) && [ ! "${NFS_MOUNT_SKIP}" ]; do
     sleep 5
     mount ${NFS_MOUNT_POINT}
 done
